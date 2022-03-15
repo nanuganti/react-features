10d4c2fb200ae7864e87418a35fe30edd4cab63b
Narahari Anuganti <nanuganti@ebsco.com>
2022-03-15 19:59:21 -0400

    feat: random api call and display json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { React, useState } from "react";
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  const [userData, setUserData] = useState()
+  const fetchUsers = async () => {
+    try {
+      const res = await axios.get("https://randomuser.me/api");
+      setUserData(JSON.stringify(res));
+    } catch (err) {
+      console.log("thereis an error", err);
+    }
+  };
+  // useEffect(async () => {
+  //   try {
+  //     const res = await axios.get("https://randomuser.me/api");
+  //     setUserData(JSON.stringify(res));
+  //   } catch (err) {
+  //     console.log("thereis an error", err);
+  //   }
+  // }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    //https://randomuser.me/api
+    <div div className="App" >
+      <header>
+        Counter App {counter}
       </header>
+      <button onClick={() => { setCounter(counter + 1) }}>increment</button>
+      <button onClick={() => { fetchUsers() }}>getUsers</button>
+      <div>
+        <p>results</p>
+        <div>{userData}</div>
+      </div>
     </div>
   );
 }
 
 export default App;
+
