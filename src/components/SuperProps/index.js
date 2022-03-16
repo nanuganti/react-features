a179d302b2a37dd26681a49d33ed4706d5d53f50
Narahari Anuganti <nanuganti@ebsco.com>
2022-03-16 19:35:13 -0400

    feat: super props usage in class component

--- src/components/SuperProps/index.js
+++ src/components/SuperProps/index.js
@@ -1,10 +1,18 @@
 import React from "react";
-const SuperProps = () => {
-    return (
-        <div>
-            <p>super props sample</p>
-        </div>
-    )
-}
 
+class SuperProps extends React.Component {
+    constructor(props) {
+        //It is used to call the constructor of its parent class. 
+        //This is required when we need to access some variables of its parent class.
+        super(props)
+        console.log(this.props)
+        // super()
+        // console.log(this.props) // Undefined 
+        // console.log(props)     // Defined Props Will Be Logged 
+    }
+
+    render() {
+        return <div>SuperProps {this.props.message}</div>;
+    }
+}
 export default SuperProps;
