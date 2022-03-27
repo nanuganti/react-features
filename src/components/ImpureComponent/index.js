b2a97d2924ba150344e63d29f874129bb06c10f1
Narahari Anuganti <nanuganti@ebsco.com>
2022-03-27 17:28:38 -0400

    feat: Add a link to pure component

--- src/components/ImpureComponent/index.js
+++ src/components/ImpureComponent/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+//https://medium.com/technofunnel/working-with-react-pure-components-166ded26ae48
 //React.PureComponent is exactly the same as React.
 //Component except that it handles the shouldComponentUpdate() method for you. 
 //When props or state changes, PureComponent will do a shallow comparison on both props and state. 
